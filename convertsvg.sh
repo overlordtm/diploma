ef548ae8a7fe77843aa4b02f3ad89aaad8dcbb60
Andraz Vrhovec <andraz@koofr.net>
2016-08-25 15:34:58 +0200

    prevodi

--- convertsvg.sh
+++ convertsvg.sh
@@ -1,8 +1,7 @@
 #!/bin/zsh
 
-for FILE in images/*.svg; do
+for FILE in svg/*.svg; do
     NOEXTNAME=${FILE:r}
-    #inkscape -D -z --file="${FILE}" --export-pdf="${NOEXTNAME}.pdf" --export-latex
     inkscape -z --verb=FitCanvasToDrawing --verb=FileSave --verb=FileClose $FILE
     inkscape $FILE --export-png=$NOEXTNAME.png --export-area-drawing -w1024
 done
